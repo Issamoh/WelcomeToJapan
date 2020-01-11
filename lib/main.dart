3701ad5dba2a8680d4a80b0bd495e6843729911e
issamoh <issammohameddaoud@gmail.com>
2020-01-11 16:20:18 +0100

    modified principal screen

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  var selectedFood = 'CAKE' ;
   @override
   Widget build(BuildContext context) {
 
@@ -50,7 +50,7 @@ children: <Widget>[
             ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
           },
       blendMode: BlendMode.dstIn,
-      child:   Image.asset('assets/japan.jpg',height:400,
+      child:   Image.asset('assets/japan.jpg',height: MediaQuery.of(context).size.height*0.45,
         width: MediaQuery.of(context).size.width,
         fit: BoxFit.cover,),
     ),
@@ -64,7 +64,7 @@ children: <Widget>[
 
   ),
   Positioned(
-    top: MediaQuery.of(context).size.height*0.4,
+    top: MediaQuery.of(context).size.height*0.3,
     //top: 250,
     left: 80,
     child:
@@ -86,8 +86,8 @@ children: <Widget>[
 
   ) ,
   Positioned(
-    top : 20 ,
-    right : 20 ,
+    top : 14 ,
+    right : 15 ,
     child: Container(
       height: 50 ,
       width : 50 ,
@@ -95,11 +95,69 @@ children: <Widget>[
         color: Colors.white,
         borderRadius: BorderRadius.circular(20)),
       child:
-         Icon(Icons.menu) ,
+         Icon(Icons.menu
+         ) ,
       ),
     ),
-      ],
+  Positioned(
+    top : 15 ,
+    right : 15 ,
+    child: Container(
+      height: 15,
+      width: 15,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(20)
+      ),
+    ),
   )
+      ],
+  ),
+     Padding(
+       padding: EdgeInsets.symmetric(horizontal: 20),
+       child: Container(
+         height: 50,
+         decoration: BoxDecoration(color: Colors.grey.shade900,borderRadius: BorderRadius.circular(5)),
+         child: TextField(
+           decoration: InputDecoration(
+             border: InputBorder.none,
+             prefixIcon: Icon(Icons.search, color: Colors.white.withOpacity(0.5)),
+             hintText: 'Tap to search ...',
+             hintStyle: TextStyle(color: Colors.grey)
+           ),
+           style: TextStyle(color: Colors.grey),
+         ),
+       ),
+
+     ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buttonBuilder('PIZZA'.toUpperCase(), Icons.local_pizza),
+              _buttonBuilder('cake'.toUpperCase(), Icons.cake),
+              _buttonBuilder('COFFEE'.toUpperCase(), Icons.local_cafe)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buttonBuilder('PIZZA1'.toUpperCase(), Icons.local_pizza),
+              _buttonBuilder('cake1'.toUpperCase(), Icons.cake),
+              _buttonBuilder('COFFEE1'.toUpperCase(), Icons.local_cafe)
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buttonBuilder('PIZZA2'.toUpperCase(), Icons.local_pizza),
+              _buttonBuilder('cake2'.toUpperCase(), Icons.cake),
+              _buttonBuilder('COFFEE2'.toUpperCase(), Icons.local_cafe)
+            ],
+          ),
+
 ]
   )
 
@@ -110,5 +168,37 @@ children: <Widget>[
 
 
 
+  }
+  Widget _buttonBuilder(foodName, foodIcon) {
+    return InkWell(
+      splashColor: Colors.transparent,
+      onTap: () {
+        setState(() {
+          selectedFood = foodName;
+        });
+      },
+      child: AnimatedContainer(
+        curve: Curves.easeIn,
+        duration: Duration(milliseconds: 300),
+        height: (selectedFood == foodName) ? MediaQuery.of(context).size.height*0.5/3 : MediaQuery.of(context).size.height*0.4/3*0.75,
+
+        width: (selectedFood== foodName) ? MediaQuery.of(context).size.width/3 : MediaQuery.of(context).size.width/3*0.75 ,
+
+        color: (selectedFood== foodName) ? Color(0xffe74c3c) : Colors.transparent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+                Icon(foodIcon,color: Colors.white,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(foodName, style: TextStyle(color: Colors.white),)
+          ],
+        ),
+      )
+      ,
+
+    );
   }
 }
